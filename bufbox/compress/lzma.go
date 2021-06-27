dd3ac334a483044a3e4c6b3edf51ce7f23ce97f8
snowmerak <go_dev@kakao.com>
2021-06-27 16:10:31 +0900

    Create lzma.go

---
+++ bufbox/compress/lzma.go
@@ -0,0 +1,43 @@
+package compress
+
+import (
+	"bytes"
+
+	"github.com/ulikunitz/xz/lzma"
+)
+
+func WriteLZMA2(data []byte) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	w, err := lzma.NewWriter2(buf)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := w.Write(data); err != nil {
+		return nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func ReadLZMA2(data []byte) ([]byte, error) {
+	red := bytes.NewBuffer(data)
+	r, err := lzma.NewReader2(red)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(nil)
+	temp := make([]byte, 4096)
+	for !r.EOS() {
+		n, err := r.Read(temp)
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			return nil, err
+		}
+		buf.Write(temp[:n])
+	}
+	return buf.Bytes(), nil
+}
