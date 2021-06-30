86f933487d1f8a8263ed70ecf7a3a469a4839a96
snowmerak <go_dev@kakao.com>
2021-06-30 17:29:08 +0900

    Make bufbox to pointer

--- bufbox/box.go
+++ bufbox/box.go
@@ -1,7 +1,59 @@
 package bufbox
 
+import (
+	"alcor/bufbox/compress"
+	"alcor/bufbox/csset"
+	"encoding/hex"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
 type BufBox struct {
 	directory         string
 	compressionWriter func([]byte) ([]byte, error)
 	compressionReader func([]byte) ([]byte, error)
+	cacheSet          csset.CSSet
+}
+
+var BasisCompression = compress.Brotli
+
+func (b *BufBox) nameToPath(name []byte) string {
+	return filepath.Join(b.directory, hex.EncodeToString(name))
+}
+
+func (b *BufBox) Exist(name []byte) bool {
+	path := b.nameToPath(name)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func (b *BufBox) Get(name []byte) ([]byte, error) {
+	content, err := os.ReadFile(b.nameToPath(name))
+	if err != nil {
+		return nil, err
+	}
+	if b.compressionReader != nil {
+		content, err = b.compressionReader(content)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return content, nil
+}
+
+func (b *BufBox) Put(name []byte, content []byte) error {
+	if b.Exist(name) {
+		return errors.New("BufBox: already key")
+	}
+	if b.compressionWriter != nil {
+		err := error(nil)
+		content, err = b.compressionWriter(content)
+		if err != nil {
+			return err
+		}
+	}
+	return os.WriteFile(b.nameToPath(name), content, 0o770)
 }
--- bufbox/builder.go
+++ bufbox/builder.go
@@ -1,19 +1,22 @@
 package bufbox
 
+import "alcor/bufbox/csset"
+
 type BufBoxWrapper struct {
-	bufBox BufBox
+	bufBox *BufBox
 }
 
-func (b BufBoxWrapper) Unwrap() BufBox {
+func (b *BufBoxWrapper) Unwrap() *BufBox {
+	b.bufBox.cacheSet = csset.CSSet{}
 	return b.bufBox
 }
 
-func (b BufBoxWrapper) SetCompression(comp func() (func([]byte) ([]byte, error), func([]byte) ([]byte, error))) BufBoxWrapper {
+func (b *BufBoxWrapper) SetCompression(comp func() (func([]byte) ([]byte, error), func([]byte) ([]byte, error))) *BufBoxWrapper {
 	b.bufBox.compressionWriter, b.bufBox.compressionReader = comp()
 	return b
 }
 
-func (b BufBoxWrapper) SetDirectory(dir string) BufBoxWrapper {
+func (b *BufBoxWrapper) SetDirectory(dir string) *BufBoxWrapper {
 	b.bufBox.directory = dir
 	return b
 }
