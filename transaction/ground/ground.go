fddfe9862bd1055ece6749e04d99656fda811a5c
snowmerak <go_dev@kakao.com>
2021-07-05 21:09:54 +0900

    delete chain and add ground and pyramid

--- transaction/ground/ground.go
+++ transaction/ground/ground.go
@@ -1,15 +1,14 @@
-package chain
+package ground
 
 import (
 	"alcor/bufbox"
 	"alcor/transaction"
-	"math/rand"
 	"path/filepath"
 
 	"google.golang.org/protobuf/proto"
 )
 
-var Path = filepath.Join(".", "db", "tangle")
+var Path = filepath.Join(".", "db", "transactions")
 
 var store *bufbox.BufBox
 
@@ -35,31 +34,4 @@ func Get(hash []byte) (*transaction.Transaction, error) {
 		return nil, err
 	}
 	return t, nil
-}
-
-func GetRandoms() (*transaction.Transaction, *transaction.Transaction, error) {
-	list, err := store.List()
-	if err != nil {
-		return nil, nil, err
-	}
-	i, j := rand.Intn(len(list)), rand.Intn(len(list))
-	if len(list) == 1 {
-		t1, err := Get(list[i])
-		if err != nil {
-			return nil, nil, err
-		}
-		return t1, nil, err
-	}
-	for i != j {
-		j = rand.Intn(len(list))
-	}
-	t1, err := Get(list[i])
-	if err != nil {
-		return nil, nil, err
-	}
-	t2, err := Get(list[j])
-	if err != nil {
-		return nil, nil, err
-	}
-	return t1, t2, err
 }
---
+++ transaction/pyramid/pyramid.go
@@ -0,0 +1,37 @@
+package pyramid
+
+import (
+	"alcor/bufbox"
+	"alcor/transaction"
+	"path/filepath"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var Path = filepath.Join(".", "db", "pairs")
+
+var store *bufbox.BufBox
+
+func init() {
+	store = new(bufbox.BufBoxWrapper).SetCompression(bufbox.BasisCompression).SetDirectory(Path).Unwrap()
+}
+
+func Put(p *transaction.Pair) error {
+	bs, err := proto.Marshal(p)
+	if err != nil {
+		return err
+	}
+	return store.Put(p.Hash, bs)
+}
+
+func Get(hash []byte) (*transaction.Pair, error) {
+	bs, err := store.Get(hash)
+	if err != nil {
+		return nil, err
+	}
+	p := new(transaction.Pair)
+	if err := proto.Unmarshal(bs, p); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
