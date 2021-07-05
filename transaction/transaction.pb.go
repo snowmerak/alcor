515befbe33c4c8a2c647091cfedf75409a72097a
snowmerak <go_dev@kakao.com>
2021-07-05 21:09:22 +0900

    Update transaction.pb.go

--- transaction/transaction.pb.go
+++ transaction/transaction.pb.go
@@ -20,204 +20,23 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type GetRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Hash []byte `protobuf:"bytes,1,opt,name=Hash,proto3" json:"Hash,omitempty"`
-}
-
-func (x *GetRequest) Reset() {
-	*x = GetRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_protobuf_transaction_proto_msgTypes[0]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *GetRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*GetRequest) ProtoMessage() {}
-
-func (x *GetRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_protobuf_transaction_proto_msgTypes[0]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use GetRequest.ProtoReflect.Descriptor instead.
-func (*GetRequest) Descriptor() ([]byte, []int) {
-	return file_protobuf_transaction_proto_rawDescGZIP(), []int{0}
-}
-
-func (x *GetRequest) GetHash() []byte {
-	if x != nil {
-		return x.Hash
-	}
-	return nil
-}
-
-type EnrollRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Message []byte `protobuf:"bytes,1,opt,name=Message,proto3" json:"Message,omitempty"`
-	From    []byte `protobuf:"bytes,2,opt,name=From,proto3" json:"From,omitempty"`
-}
-
-func (x *EnrollRequest) Reset() {
-	*x = EnrollRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_protobuf_transaction_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *EnrollRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*EnrollRequest) ProtoMessage() {}
-
-func (x *EnrollRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_protobuf_transaction_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use EnrollRequest.ProtoReflect.Descriptor instead.
-func (*EnrollRequest) Descriptor() ([]byte, []int) {
-	return file_protobuf_transaction_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *EnrollRequest) GetMessage() []byte {
-	if x != nil {
-		return x.Message
-	}
-	return nil
-}
-
-func (x *EnrollRequest) GetFrom() []byte {
-	if x != nil {
-		return x.From
-	}
-	return nil
-}
-
-type EnrollResponse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Hash      []byte   `protobuf:"bytes,1,opt,name=Hash,proto3" json:"Hash,omitempty"`
-	Message   []byte   `protobuf:"bytes,2,opt,name=Message,proto3" json:"Message,omitempty"`
-	Timestamp []byte   `protobuf:"bytes,3,opt,name=Timestamp,proto3" json:"Timestamp,omitempty"`
-	From      []byte   `protobuf:"bytes,4,opt,name=From,proto3" json:"From,omitempty"`
-	Befores   [][]byte `protobuf:"bytes,5,rep,name=Befores,proto3" json:"Befores,omitempty"`
-}
-
-func (x *EnrollResponse) Reset() {
-	*x = EnrollResponse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_protobuf_transaction_proto_msgTypes[2]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *EnrollResponse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*EnrollResponse) ProtoMessage() {}
-
-func (x *EnrollResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_protobuf_transaction_proto_msgTypes[2]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use EnrollResponse.ProtoReflect.Descriptor instead.
-func (*EnrollResponse) Descriptor() ([]byte, []int) {
-	return file_protobuf_transaction_proto_rawDescGZIP(), []int{2}
-}
-
-func (x *EnrollResponse) GetHash() []byte {
-	if x != nil {
-		return x.Hash
-	}
-	return nil
-}
-
-func (x *EnrollResponse) GetMessage() []byte {
-	if x != nil {
-		return x.Message
-	}
-	return nil
-}
-
-func (x *EnrollResponse) GetTimestamp() []byte {
-	if x != nil {
-		return x.Timestamp
-	}
-	return nil
-}
-
-func (x *EnrollResponse) GetFrom() []byte {
-	if x != nil {
-		return x.From
-	}
-	return nil
-}
-
-func (x *EnrollResponse) GetBefores() [][]byte {
-	if x != nil {
-		return x.Befores
-	}
-	return nil
-}
-
 type Transaction struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Hash      []byte `protobuf:"bytes,1,opt,name=Hash,proto3" json:"Hash,omitempty"`
-	Message   []byte `protobuf:"bytes,2,opt,name=Message,proto3" json:"Message,omitempty"`
-	Timestamp []byte `protobuf:"bytes,3,opt,name=Timestamp,proto3" json:"Timestamp,omitempty"`
-	From      []byte `protobuf:"bytes,4,opt,name=From,proto3" json:"From,omitempty"`
-	Befores   []byte `protobuf:"bytes,5,opt,name=Befores,proto3" json:"Befores,omitempty"`
+	Key       []byte `protobuf:"bytes,1,opt,name=Key,proto3" json:"Key,omitempty"`
+	Hash      []byte `protobuf:"bytes,2,opt,name=Hash,proto3" json:"Hash,omitempty"`
+	Message   []byte `protobuf:"bytes,3,opt,name=Message,proto3" json:"Message,omitempty"`
+	Timestamp []byte `protobuf:"bytes,4,opt,name=Timestamp,proto3" json:"Timestamp,omitempty"`
+	From      []byte `protobuf:"bytes,5,opt,name=From,proto3" json:"From,omitempty"`
 	Signature []byte `protobuf:"bytes,6,opt,name=Signature,proto3" json:"Signature,omitempty"`
 }
 
 func (x *Transaction) Reset() {
 	*x = Transaction{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_protobuf_transaction_proto_msgTypes[3]
+		mi := &file_protobuf_transaction_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -230,7 +49,7 @@ func (x *Transaction) String() string {
 func (*Transaction) ProtoMessage() {}
 
 func (x *Transaction) ProtoReflect() protoreflect.Message {
-	mi := &file_protobuf_transaction_proto_msgTypes[3]
+	mi := &file_protobuf_transaction_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -243,7 +62,14 @@ func (x *Transaction) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Transaction.ProtoReflect.Descriptor instead.
 func (*Transaction) Descriptor() ([]byte, []int) {
-	return file_protobuf_transaction_proto_rawDescGZIP(), []int{3}
+	return file_protobuf_transaction_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *Transaction) GetKey() []byte {
+	if x != nil {
+		return x.Key
+	}
+	return nil
 }
 
 func (x *Transaction) GetHash() []byte {
@@ -274,16 +100,64 @@ func (x *Transaction) GetFrom() []byte {
 	return nil
 }
 
-func (x *Transaction) GetBefores() []byte {
+func (x *Transaction) GetSignature() []byte {
 	if x != nil {
-		return x.Befores
+		return x.Signature
 	}
 	return nil
 }
 
-func (x *Transaction) GetSignature() []byte {
+type Pair struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Hash    []byte   `protobuf:"bytes,1,opt,name=Hash,proto3" json:"Hash,omitempty"`
+	Insides [][]byte `protobuf:"bytes,2,rep,name=Insides,proto3" json:"Insides,omitempty"`
+}
+
+func (x *Pair) Reset() {
+	*x = Pair{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protobuf_transaction_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Pair) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Pair) ProtoMessage() {}
+
+func (x *Pair) ProtoReflect() protoreflect.Message {
+	mi := &file_protobuf_transaction_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Pair.ProtoReflect.Descriptor instead.
+func (*Pair) Descriptor() ([]byte, []int) {
+	return file_protobuf_transaction_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *Pair) GetHash() []byte {
 	if x != nil {
-		return x.Signature
+		return x.Hash
+	}
+	return nil
+}
+
+func (x *Pair) GetInsides() [][]byte {
+	if x != nil {
+		return x.Insides
 	}
 	return nil
 }
@@ -293,32 +167,20 @@ var File_protobuf_transaction_proto protoreflect.FileDescriptor
 var file_protobuf_transaction_proto_rawDesc = []byte{
 	0x0a, 0x1a, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x74, 0x72, 0x61, 0x6e, 0x73,
 	0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0b, 0x74, 0x72,
-	0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x20, 0x0a, 0x0a, 0x47, 0x65, 0x74,
-	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x48, 0x61, 0x73, 0x68, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x48, 0x61, 0x73, 0x68, 0x22, 0x3d, 0x0a, 0x0d, 0x45,
-	0x6e, 0x72, 0x6f, 0x6c, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x18, 0x0a, 0x07,
-	0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x07, 0x4d,
-	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x18, 0x02,
-	0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x22, 0x8a, 0x01, 0x0a, 0x0e, 0x45,
-	0x6e, 0x72, 0x6f, 0x6c, 0x6c, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x12, 0x0a,
-	0x04, 0x48, 0x61, 0x73, 0x68, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x48, 0x61, 0x73,
-	0x68, 0x12, 0x18, 0x0a, 0x07, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01,
-	0x28, 0x0c, 0x52, 0x07, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x1c, 0x0a, 0x09, 0x54,
-	0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09,
-	0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x12, 0x12, 0x0a, 0x04, 0x46, 0x72, 0x6f,
-	0x6d, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x12, 0x18, 0x0a,
-	0x07, 0x42, 0x65, 0x66, 0x6f, 0x72, 0x65, 0x73, 0x18, 0x05, 0x20, 0x03, 0x28, 0x0c, 0x52, 0x07,
-	0x42, 0x65, 0x66, 0x6f, 0x72, 0x65, 0x73, 0x22, 0xa5, 0x01, 0x0a, 0x0b, 0x54, 0x72, 0x61, 0x6e,
-	0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x12, 0x0a, 0x04, 0x48, 0x61, 0x73, 0x68, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x48, 0x61, 0x73, 0x68, 0x12, 0x18, 0x0a, 0x07, 0x4d,
-	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x07, 0x4d, 0x65,
-	0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x1c, 0x0a, 0x09, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61,
-	0x6d, 0x70, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74,
-	0x61, 0x6d, 0x70, 0x12, 0x12, 0x0a, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x18, 0x04, 0x20, 0x01, 0x28,
-	0x0c, 0x52, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x12, 0x18, 0x0a, 0x07, 0x42, 0x65, 0x66, 0x6f, 0x72,
-	0x65, 0x73, 0x18, 0x05, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x07, 0x42, 0x65, 0x66, 0x6f, 0x72, 0x65,
-	0x73, 0x12, 0x1c, 0x0a, 0x09, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18, 0x06,
-	0x20, 0x01, 0x28, 0x0c, 0x52, 0x09, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x42,
+	0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x9d, 0x01, 0x0a, 0x0b, 0x54, 0x72,
+	0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x10, 0x0a, 0x03, 0x4b, 0x65, 0x79,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x03, 0x4b, 0x65, 0x79, 0x12, 0x12, 0x0a, 0x04, 0x48,
+	0x61, 0x73, 0x68, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x48, 0x61, 0x73, 0x68, 0x12,
+	0x18, 0x0a, 0x07, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0c,
+	0x52, 0x07, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x1c, 0x0a, 0x09, 0x54, 0x69, 0x6d,
+	0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09, 0x54, 0x69,
+	0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x12, 0x12, 0x0a, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x18,
+	0x05, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x46, 0x72, 0x6f, 0x6d, 0x12, 0x1c, 0x0a, 0x09, 0x53,
+	0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18, 0x06, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09,
+	0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x22, 0x34, 0x0a, 0x04, 0x50, 0x61, 0x69,
+	0x72, 0x12, 0x12, 0x0a, 0x04, 0x48, 0x61, 0x73, 0x68, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52,
+	0x04, 0x48, 0x61, 0x73, 0x68, 0x12, 0x18, 0x0a, 0x07, 0x49, 0x6e, 0x73, 0x69, 0x64, 0x65, 0x73,
+	0x18, 0x02, 0x20, 0x03, 0x28, 0x0c, 0x52, 0x07, 0x49, 0x6e, 0x73, 0x69, 0x64, 0x65, 0x73, 0x42,
 	0x0f, 0x5a, 0x0d, 0x2e, 0x2f, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e,
 	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
@@ -335,12 +197,10 @@ func file_protobuf_transaction_proto_rawDescGZIP() []byte {
 	return file_protobuf_transaction_proto_rawDescData
 }
 
-var file_protobuf_transaction_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_protobuf_transaction_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_protobuf_transaction_proto_goTypes = []interface{}{
-	(*GetRequest)(nil),     // 0: transaction.GetRequest
-	(*EnrollRequest)(nil),  // 1: transaction.EnrollRequest
-	(*EnrollResponse)(nil), // 2: transaction.EnrollResponse
-	(*Transaction)(nil),    // 3: transaction.Transaction
+	(*Transaction)(nil), // 0: transaction.Transaction
+	(*Pair)(nil),        // 1: transaction.Pair
 }
 var file_protobuf_transaction_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -357,7 +217,7 @@ func file_protobuf_transaction_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_protobuf_transaction_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*GetRequest); i {
+			switch v := v.(*Transaction); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -369,31 +229,7 @@ func file_protobuf_transaction_proto_init() {
 			}
 		}
 		file_protobuf_transaction_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*EnrollRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_protobuf_transaction_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*EnrollResponse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_protobuf_transaction_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Transaction); i {
+			switch v := v.(*Pair); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -411,7 +247,7 @@ func file_protobuf_transaction_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protobuf_transaction_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
