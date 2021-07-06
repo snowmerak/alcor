8a56cf814973d84fddbbd88bac2433f2e7c9ca8e
snowmerak <go_dev@kakao.com>
2021-07-07 00:16:55 +0900

    Create book.go

---
+++ client/book/book.go
@@ -0,0 +1,37 @@
+package book
+
+import (
+	"alcor/bufbox"
+	"alcor/client"
+	"path/filepath"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var Path = filepath.Join(".", "db", "clients")
+
+var store *bufbox.BufBox
+
+func init() {
+	store = new(bufbox.BufBoxWrapper).SetCompression(bufbox.BasisCompression).SetDirectory(Path).Unwrap()
+}
+
+func Put(c *client.Client) error {
+	bs, err := proto.Marshal(c)
+	if err != nil {
+		return err
+	}
+	return store.Put(c.ID, bs)
+}
+
+func Get(hash []byte) (*client.Client, error) {
+	bs, err := store.Get(hash)
+	if err != nil {
+		return nil, err
+	}
+	c := new(client.Client)
+	if err := proto.Unmarshal(bs, c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
