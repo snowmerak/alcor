94b2fa8817feac470bb7f531c6dfe8bc0275764a
snowmerak <go_dev@kakao.com>
2021-07-10 00:26:30 +0900

    Update App.svelte

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,30 +1,56 @@
 <script>
-	export let name;
+	import axios from 'axios'
+import { loop_guard } from 'svelte/internal';
+
+	let selectedMenu = 0
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<ul uk-tab>
+	<li class={selectedMenu==0?"uk-active":""}><a on:click={() => selectedMenu = 0} href=".">Wallet</a></li>
+	<li class={selectedMenu==1?"uk-active":""}><a on:click={() => selectedMenu = 1} href=".">Clients</a></li>
+	<li class={selectedMenu==2?"uk-active":""}><a on:click={() => selectedMenu = 2} href=".">Wallet</a></li>
+	<li class={selectedMenu==3?"uk-active":""}><a on:click={() => selectedMenu = 3} href=".">Transactions</a></li>
+    <li class={selectedMenu==4?"uk-active":""}><a on:click={() => selectedMenu = 4} href=".">Pairs</a></li>
+</ul>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+{#if selectedMenu == 0}
+	<!-- svelte-ignore empty-block -->
+	<ul uk-accordion="multiple: true">
+	{#await axios.get("/wallet/get").then(res => res.data)}
+	{:then accounts}
+		{#each accounts as account}
+		<li>
+			<a class="uk-accordion-title" href=".">{account.ID}</a>
+			<div class="uk-accordion-content">
+				<p>PrivateKey: {account.PrivateKey}</p>
+				<p>PublicKey: {account.PublicKey}</p>
+			</div>
+		</li>
+		{/each}
+	{/await}
+	</ul>
+{:else if selectedMenu == 1}
+	<!-- svelte-ignore empty-block -->
+	<ul uk-accordion="multiple: true">
+		{#await axios.get("/client/get").then(res => res.data)}
+		{:then clients}
+			{#each clients as client}
+			<li>
+				<a class="uk-accordion-title" href=".">{client.ID}</a>
+				<div class="uk-accordion-content">
+					<p>PublicKey: {client.PublicKey}</p>
+				</div>
+			</li>
+			{/each}
+		{/await}
+		</ul>
+{/if}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<!-- <ul uk-accordion="multiple: true">
+    <li class="uk-open">
+        <a class="uk-accordion-title" href=".">Item 1</a>
+        <div class="uk-accordion-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </li>
+</ul> -->
