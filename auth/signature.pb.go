97d04d807cd75a2c246b075d30061f40ab6d7a50
snowmerak <go_dev@kakao.com>
2021-07-09 20:15:11 +0900

    Update signature.pb.go

--- auth/signature.pb.go
+++ auth/signature.pb.go
@@ -75,6 +75,61 @@ func (x *Signature) GetS() string {
 	return ""
 }
 
+type Result struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Ok    bool   `protobuf:"varint,1,opt,name=Ok,proto3" json:"Ok,omitempty"`
+	Error []byte `protobuf:"bytes,2,opt,name=Error,proto3" json:"Error,omitempty"`
+}
+
+func (x *Result) Reset() {
+	*x = Result{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protobuf_signature_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Result) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Result) ProtoMessage() {}
+
+func (x *Result) ProtoReflect() protoreflect.Message {
+	mi := &file_protobuf_signature_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Result.ProtoReflect.Descriptor instead.
+func (*Result) Descriptor() ([]byte, []int) {
+	return file_protobuf_signature_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *Result) GetOk() bool {
+	if x != nil {
+		return x.Ok
+	}
+	return false
+}
+
+func (x *Result) GetError() []byte {
+	if x != nil {
+		return x.Error
+	}
+	return nil
+}
+
 var File_protobuf_signature_proto protoreflect.FileDescriptor
 
 var file_protobuf_signature_proto_rawDesc = []byte{
@@ -82,7 +137,10 @@ var file_protobuf_signature_proto_rawDesc = []byte{
 	0x74, 0x75, 0x72, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x04, 0x61, 0x75, 0x74, 0x68,
 	0x22, 0x27, 0x0a, 0x09, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x12, 0x0c, 0x0a,
 	0x01, 0x52, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x01, 0x52, 0x12, 0x0c, 0x0a, 0x01, 0x53,
-	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x01, 0x53, 0x42, 0x08, 0x5a, 0x06, 0x2e, 0x2f, 0x61,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x01, 0x53, 0x22, 0x2e, 0x0a, 0x06, 0x52, 0x65, 0x73,
+	0x75, 0x6c, 0x74, 0x12, 0x0e, 0x0a, 0x02, 0x4f, 0x6b, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52,
+	0x02, 0x4f, 0x6b, 0x12, 0x14, 0x0a, 0x05, 0x45, 0x72, 0x72, 0x6f, 0x72, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x0c, 0x52, 0x05, 0x45, 0x72, 0x72, 0x6f, 0x72, 0x42, 0x08, 0x5a, 0x06, 0x2e, 0x2f, 0x61,
 	0x75, 0x74, 0x68, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
@@ -98,9 +156,10 @@ func file_protobuf_signature_proto_rawDescGZIP() []byte {
 	return file_protobuf_signature_proto_rawDescData
 }
 
-var file_protobuf_signature_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_protobuf_signature_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_protobuf_signature_proto_goTypes = []interface{}{
 	(*Signature)(nil), // 0: auth.Signature
+	(*Result)(nil),    // 1: auth.Result
 }
 var file_protobuf_signature_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -128,6 +187,18 @@ func file_protobuf_signature_proto_init() {
 				return nil
 			}
 		}
+		file_protobuf_signature_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Result); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -135,7 +206,7 @@ func file_protobuf_signature_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protobuf_signature_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
