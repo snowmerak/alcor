4a345954a988024b580cad7d66fb34f16c354189
snowmerak <go_dev@kakao.com>
2021-07-10 02:15:33 +0900

    Update App.svelte

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,22 +1,32 @@
 <script>
 	import axios from 'axios'
-import { loop_guard } from 'svelte/internal';
+	import { loop_guard } from 'svelte/internal';
+	import { get } from 'svelte/store';
 
 	let selectedMenu = 0
+	let input = ""
 </script>
 
 <ul uk-tab>
 	<li class={selectedMenu==0?"uk-active":""}><a on:click={() => selectedMenu = 0} href=".">Wallet</a></li>
 	<li class={selectedMenu==1?"uk-active":""}><a on:click={() => selectedMenu = 1} href=".">Clients</a></li>
-	<li class={selectedMenu==2?"uk-active":""}><a on:click={() => selectedMenu = 2} href=".">Wallet</a></li>
-	<li class={selectedMenu==3?"uk-active":""}><a on:click={() => selectedMenu = 3} href=".">Transactions</a></li>
-    <li class={selectedMenu==4?"uk-active":""}><a on:click={() => selectedMenu = 4} href=".">Pairs</a></li>
+	<li class={selectedMenu==2?"uk-active":""}><a on:click={() => selectedMenu = 2} href=".">Transactions</a></li>
+	<li class={selectedMenu==3?"uk-active":""}><a on:click={() => selectedMenu = 3} href=".">Pairs</a></li>
+    <li class={selectedMenu==4?"uk-active":""}><a on:click={() => selectedMenu = 4} href=".">Config</a></li>
 </ul>
 
 {#if selectedMenu == 0}
+	<form class="uk-grid-small" uk-grid>
+	    <div class="uk-width-4-5">
+	        <input class="uk-input" type="text" placeholder="ID" value={input}>
+	    </div>
+	    <div class="uk-width-1-5">
+	        <button class="uk-button" on:click={async () => { let result = await axios.put("/wallet", input).then(res => res.data); alert(result) }}>Add</button>
+	    </div>
+	</form>
 	<!-- svelte-ignore empty-block -->
 	<ul uk-accordion="multiple: true">
-	{#await axios.get("/wallet/get").then(res => res.data)}
+	{#await axios.get("/wallet").then(res => res.data)}
 	{:then accounts}
 		{#each accounts as account}
 		<li>
@@ -32,7 +42,7 @@ import { loop_guard } from 'svelte/internal';
 {:else if selectedMenu == 1}
 	<!-- svelte-ignore empty-block -->
 	<ul uk-accordion="multiple: true">
-		{#await axios.get("/client/get").then(res => res.data)}
+		{#await axios.get("/client").then(res => res.data)}
 		{:then clients}
 			{#each clients as client}
 			<li>
@@ -43,7 +53,22 @@ import { loop_guard } from 'svelte/internal';
 			</li>
 			{/each}
 		{/await}
-		</ul>
+	</ul>
+{:else if selectedMenu == 4}
+	<form class="uk-form-horizontal">
+		<div class="uk-margin">
+			<!-- svelte-ignore empty-block -->
+			{#await axios.get("/config/higheraddress").then(res => res.data)}
+			{:then address}
+			<label class="uk-form-label" for="form-stacked-text">Higher Node Address</label>
+			<div class="uk-inline">
+				<!-- svelte-ignore a11y-missing-content -->
+				<a class="uk-form-icon" href="." uk-icon="icon: pencil"></a>
+				<input class="uk-input" id="form-stacked-text" type="text" value={address} placeholder="higher node address">
+			</div>
+			{/await}
+		</div>
+	</form>
 {/if}
 
 <!-- <ul uk-accordion="multiple: true">
