46c26a91bcd2a17096ea31e2e2a7feeaf75705be
snowmerak <go_dev@kakao.com>
2021-07-10 02:15:37 +0900

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"flag"
+	"io"
 	"io/fs"
 	"log"
 	"net"
@@ -15,6 +16,7 @@ import (
 	"alcor/client/book"
 	clientSocket "alcor/client/socket"
 	"alcor/client/wallet"
+	"alcor/config"
 
 	"github.com/webview/webview"
 )
@@ -24,7 +26,6 @@ var web embed.FS
 
 func main() {
 	isMaster := flag.Bool("master", false, "")
-	clientEnrollTest := flag.Bool("client_enroll", false, "")
 	flag.Parse()
 
 	// port, err := GetFreePort()
@@ -34,59 +35,79 @@ func main() {
 	// fmt.Println(port)
 	port := "9999"
 
-	if !(*clientEnrollTest) {
-		go func() {
-			public, err := fs.Sub(web, "ui/public")
-			if err != nil {
-				log.Fatal(err)
-			}
+	go func() {
+		public, err := fs.Sub(web, "ui/public")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.FS(public))))
+		http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.FS(public))))
 
-			if *isMaster {
-				http.HandleFunc("/client/enroll", func(rw http.ResponseWriter, r *http.Request) {
-					if err := clientSocket.EnrollServer(rw, r); err != nil {
-						log.Panicln(err)
-					}
-				})
-			}
+		if *isMaster {
+			http.HandleFunc("/client/enroll", func(rw http.ResponseWriter, r *http.Request) {
+				if err := clientSocket.EnrollServer(rw, r); err != nil {
+					log.Panicln(err)
+				}
+			})
+		}
 
-			http.HandleFunc("/wallet/get", func(rw http.ResponseWriter, r *http.Request) {
+		http.HandleFunc("/wallet", func(rw http.ResponseWriter, r *http.Request) {
+			switch strings.ToUpper(r.Method) {
+			case "GET":
 				rw.WriteHeader(202)
 				accounts, _ := wallet.List()
 				buffer := bytes.NewBuffer(nil)
 				encoder := json.NewEncoder(buffer)
 				encoder.Encode(accounts)
 				rw.Write(buffer.Bytes())
-			})
+			case "PUT":
+				rw.WriteHeader(202)
+				bs, err := io.ReadAll(r.Body)
+				if err != nil {
+					rw.Write(bs)
+					return
+				}
+				clientSocket.EnrollClient("ws://"+config.HigherAddress+":"+port+"/client/enroll", string(bs))
+			}
+		})
 
-			http.HandleFunc("/client/get", func(rw http.ResponseWriter, r *http.Request) {
+		http.HandleFunc("/client", func(rw http.ResponseWriter, r *http.Request) {
+			switch strings.ToUpper(r.Method) {
+			case "GET":
 				rw.WriteHeader(202)
 				clients, _ := book.List()
 				buffer := bytes.NewBuffer(nil)
 				encoder := json.NewEncoder(buffer)
 				encoder.Encode(clients)
 				rw.Write(buffer.Bytes())
-			})
-
-			if err := http.ListenAndServe(":"+port, nil); err != nil {
-				log.Fatal(err)
 			}
-		}()
+		})
 
-		const debug = true
-		w := webview.New(debug)
-		defer w.Destroy()
-		w.SetSize(1200, 860, webview.HintFixed)
-		w.Navigate("http://127.0.0.1:" + port + "/public/")
-		w.Run()
-	}
+		http.HandleFunc("/config/higheraddress", func(rw http.ResponseWriter, r *http.Request) {
+			switch strings.ToUpper(r.Method) {
+			case "GET":
+				rw.WriteHeader(202)
+				rw.Write([]byte(config.HigherAddress))
+			case "PUT":
+				rw.WriteHeader(202)
+			case "DELETE":
+				rw.WriteHeader(202)
+			case "PATCH":
+				rw.WriteHeader(202)
+			}
+		})
 
-	if *clientEnrollTest {
-		if err := clientSocket.EnrollClient("ws://127.0.0.1:"+port+"/client/enroll", "kym985478"); err != nil {
-			log.Println(err)
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal(err)
 		}
-	}
+	}()
+
+	const debug = true
+	w := webview.New(debug)
+	defer w.Destroy()
+	w.SetSize(800, 640, webview.HintNone)
+	w.Navigate("http://127.0.0.1:" + port + "/public/")
+	w.Run()
 }
 
 func GetFreePort() (string, error) {
