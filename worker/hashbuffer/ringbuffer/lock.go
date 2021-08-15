1f81d1687ba4c2d6cbcc977cfb046e2af35c9397
snowmerak@bitbucket.org <snowmerak@outlook.com>
2021-08-15 15:10:12 +0900

    Add hashbuffer worker

---
+++ worker/hashbuffer/ringbuffer/lock.go
@@ -0,0 +1,34 @@
+package ringbuffer
+
+import (
+	"runtime"
+	"sync/atomic"
+)
+
+//This code is created my lemon-mint.
+
+//TLock ...
+type TLock struct {
+	lock uint32
+}
+
+// Lock acquires the lock
+func (l *TLock) Lock() {
+	for {
+		if l.TryLock() {
+			break // escape loop
+		} else {
+			runtime.Gosched() // yeilds to other goroutines
+		}
+	}
+}
+
+// TryLock tries to acquire the lock
+func (l *TLock) TryLock() (locked bool) {
+	return atomic.CompareAndSwapUint32(&l.lock, 0, 1)
+}
+
+// Unlock releases the lock
+func (l *TLock) Unlock() {
+	atomic.StoreUint32(&l.lock, 0) // release lock
+}
