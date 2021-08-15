fa4dd9044d2ad1902faaeb8e044d0be6af634b2b
snowmerak@bitbucket.org <snowmerak@outlook.com>
2021-08-15 15:10:22 +0900

    Add sessions worker

---
+++ worker/sessions/sessions.go
@@ -0,0 +1,34 @@
+package sessions
+
+import (
+	"log"
+
+	"github.com/bradfitz/gomemcache/memcache"
+)
+
+var store *memcache.Client
+
+func init() {
+	store = memcache.New("seesions:11211")
+	if store == nil {
+		log.Fatal("cannot connect sessions")
+	}
+}
+
+func Set(key string, value []byte) error {
+	return store.Set(&memcache.Item{
+		Key: key, Value: value,
+	})
+}
+
+func Get(key string) ([]byte, error) {
+	item, err := store.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	return item.Value, nil
+}
+
+func Delete(key string) error {
+	return store.Delete(key)
+}
