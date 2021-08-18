8aa6f120f2e0ef5104b25cd05f21734560b9828c
snowmerak <snowmerak@outlook.com>
2021-08-18 15:22:41 +0900

    Update stats.pb.go

--- model/stats/stats.pb.go
+++ model/stats/stats.pb.go
@@ -171,6 +171,53 @@ func (x *Data) GetVoterID() []byte {
 	return nil
 }
 
+type List struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Data []string `protobuf:"bytes,1,rep,name=Data,proto3" json:"Data,omitempty"`
+}
+
+func (x *List) Reset() {
+	*x = List{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_protobuf_stats_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *List) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*List) ProtoMessage() {}
+
+func (x *List) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_protobuf_stats_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use List.ProtoReflect.Descriptor instead.
+func (*List) Descriptor() ([]byte, []int) {
+	return file_proto_protobuf_stats_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *List) GetData() []string {
+	if x != nil {
+		return x.Data
+	}
+	return nil
+}
+
 var File_proto_protobuf_stats_proto protoreflect.FileDescriptor
 
 var file_proto_protobuf_stats_proto_rawDesc = []byte{
@@ -199,8 +246,10 @@ var file_proto_protobuf_stats_proto_rawDesc = []byte{
 	0x65, 0x6f, 0x6c, 0x6f, 0x67, 0x79, 0x18, 0x0d, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x49, 0x64,
 	0x65, 0x6f, 0x6c, 0x6f, 0x67, 0x79, 0x12, 0x18, 0x0a, 0x07, 0x56, 0x6f, 0x74, 0x65, 0x72, 0x49,
 	0x44, 0x18, 0x0e, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x07, 0x56, 0x6f, 0x74, 0x65, 0x72, 0x49, 0x44,
-	0x42, 0x0d, 0x5a, 0x0b, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2f, 0x73, 0x74, 0x61, 0x74, 0x73, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x1a, 0x0a, 0x04, 0x4c, 0x69, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x44, 0x61, 0x74, 0x61,
+	0x18, 0x01, 0x20, 0x03, 0x28, 0x09, 0x52, 0x04, 0x44, 0x61, 0x74, 0x61, 0x42, 0x0d, 0x5a, 0x0b,
+	0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2f, 0x73, 0x74, 0x61, 0x74, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
@@ -215,9 +264,10 @@ func file_proto_protobuf_stats_proto_rawDescGZIP() []byte {
 	return file_proto_protobuf_stats_proto_rawDescData
 }
 
-var file_proto_protobuf_stats_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_proto_protobuf_stats_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_proto_protobuf_stats_proto_goTypes = []interface{}{
 	(*Data)(nil), // 0: stats.Data
+	(*List)(nil), // 1: stats.List
 }
 var file_proto_protobuf_stats_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -245,6 +295,18 @@ func file_proto_protobuf_stats_proto_init() {
 				return nil
 			}
 		}
+		file_proto_protobuf_stats_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*List); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -252,7 +314,7 @@ func file_proto_protobuf_stats_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_protobuf_stats_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
