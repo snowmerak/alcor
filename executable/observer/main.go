053fa676e2ba448e513fc031be76004601903a88
snowmerak <snowmerak@outlook.com>
2021-08-20 19:17:17 +0900

    Init Observer

--- executable/observer/main.go
+++ executable/observer/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"embed"
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+)
+
+//go:embed public
+var public embed.FS
 
 func main() {
 	app := fiber.New()
+
+	app.Use(filesystem.New(filesystem.Config{
+		Root: http.FS(public),
+	}))
+
+	if err := app.Listen(":10000"); err != nil {
+		log.Fatal(err)
+	}
 }
