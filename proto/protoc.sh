e5598f0c3f7dd5e3ef6347124c35cdb16ed32959
snowmerak <snowmerak@outlook.com>
2021-08-21 12:49:11 +0900

    Update protobuf

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -2,3 +2,7 @@
 
 protoc --go_out=./ proto/protobuf/*.proto
 protoc --dart_out=./client/lib proto/protobuf/*.proto
+
+cp proto/protobuf/*.proto ./observer/src/model
+pb-rs ./observer/src/model/*.proto
+rm ./observer/src/model/*.proto
