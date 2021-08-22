8d08243ac4da68f7658086b868390b640932807a
snowmerak <snowmerak@outlook.com>
2021-08-23 00:27:39 +0900

    Update ptoro, model

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -2,7 +2,4 @@
 
 protoc --go_out=./ proto/protobuf/*.proto
 protoc --dart_out=./client/lib proto/protobuf/*.proto
-
-cp proto/protobuf/*.proto ./observer/src/model
-pb-rs ./observer/src/model/*.proto
-rm ./observer/src/model/*.proto
+protoc --python_out=./observer/src proto/protobuf/*proto
