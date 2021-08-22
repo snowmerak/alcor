5ee1254a09625dbb132b07292c57b3e688465c9e
snowmerak <snowmerak@outlook.com>
2021-08-23 00:30:29 +0900

    뭐가 수정된거지?

--- service/survey/get.go
+++ service/survey/get.go
@@ -1,8 +1,12 @@
 package survey
 
 import (
+	"alcor/db"
 	"alcor/model/stats"
 	"alcor/static"
+	"alcor/worker/rdb"
+	"context"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/valyala/fasthttp"
@@ -72,4 +76,45 @@ func GetIdeologys(c *fiber.Ctx) error {
 
 	c.SendStatus(fasthttp.StatusOK)
 	return c.Send(data)
+}
+
+func GetMaxCount(c *fiber.Ctx) error {
+	data, err := rdb.GetSurveyIndex()
+	if err != nil {
+		data = 0
+	}
+	c.SendStatus(fasthttp.StatusOK)
+	return c.SendString(strconv.FormatUint(data, 10))
+}
+
+func GetIndex(c *fiber.Ctx) error {
+	n, err := strconv.ParseUint(c.Params("number"), 10, 64)
+	if err != nil {
+		return c.SendStatus(fasthttp.StatusBadRequest)
+	}
+	data := new(db.Data)
+	data.Number = int64(n)
+	if err := db.SelectData(context.Background(), data); err != nil {
+		return c.SendStatus(fasthttp.StatusBadRequest)
+	}
+	survey := new(stats.Data)
+	survey.Age = int32(data.Age)
+	survey.Candidate = data.Candidate
+	survey.Divorced = data.Divorced
+	survey.Education = data.Education
+	survey.Gender = data.Gender
+	survey.HasCar = data.HasCar
+	survey.HasDebt = data.HasDebt
+	survey.HouseType = data.HouseType
+	survey.Ideology = data.Ideology
+	survey.Job = data.Job
+	survey.Married = data.Married
+	survey.Region = data.Region
+	survey.Salary = int32(data.Salary)
+	bs, err := proto.Marshal(survey)
+	if err != nil {
+		return c.SendStatus(fasthttp.StatusConflict)
+	}
+	c.SendStatus(fasthttp.StatusOK)
+	return c.Send(bs)
 }
