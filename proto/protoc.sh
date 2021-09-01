8c20c7b50cb2a0db8c06f9807de18b258d204927
snowmerak <snowmerak@outlook.com>
2021-09-01 18:45:50 +0900

    Update protoc.sh

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -3,3 +3,4 @@
 protoc --go_out=./ proto/protobuf/*.proto
 protoc --dart_out=./client/lib proto/protobuf/*.proto
 protoc --dart_out=./admin/lib proto/protobuf/*.proto
+protoc --python_out=./edgepy proto/protobuf/*.proto
